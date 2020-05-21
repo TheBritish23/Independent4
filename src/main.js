f6c93fa64c1cacdd21c2ffcf20585e8b2cd8b845
Soro Cat <sorousha326@gmail.com>
2020-05-21 10:16:18 -0700

    fix indentation

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {userAge} from "./age.js";
+import {age} from "./age.js";
 
 $(document).ready(function(){
   $("form#Age1").submit(function(event) {
@@ -10,17 +10,17 @@ $(document).ready(function(){
     const userAge = parseInt($("#userAge").val());
     if (Math.ceil(userAge)-userAge) {
       alert("Invalid Input, *****!");
-} else {
-  const newAge = new age(userAge);
-  $("#mercuryYears").text(newAge.mercuryYears());
-  $("#venusYears").text(newAge.venusYears());
-  $("#marsYears").text(newAge.marsYears());
-  $("#jupiterYears").text(newAge.jupiterYears());
-  if (userAge < 99) {
-    $("#young").text(newAge.lifeExpectancy(userAge));
-  }else{
+    } else {
+      const newAge = new age(userAge);
+      $("#mercuryYears").text(newAge.mercuryYears());
+      $("#venusYears").text(newAge.venusYears());
+      $("#marsYears").text(newAge.marsYears());
+      $("#jupiterYears").text(newAge.jupiterYears());
+      if (userAge < 99) {
+        $("#young").text(newAge.lifeExpectancy(userAge));
+      }else{
     return $("#old").text(newAge.lifeExpectancy(userAge));
   }
-}
-});
+    }
+  });
 });
