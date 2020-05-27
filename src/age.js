52b5a3115c17bdee9a54cffc4f7cfdcbeb38db65
Soro Cat <sorousha326@gmail.com>
2020-05-27 11:01:01 -0700

    add values

--- src/age.js
+++ src/age.js
@@ -4,3 +4,17 @@ export function userAge(age) {
   this.mercuryYears = age;
   this.jupiterYears = age;
 }
+
+export class userAge{
+
+}
+mercuryYears(){
+  this.age = .24
+  const userAge = Math.round(this.age / this.planet);
+  return userAge;
+  }
+mercuryYears(){
+  this.age = .62
+  const userAge = Math.round(this.age / this.planet);
+  return userAge;
+  }
