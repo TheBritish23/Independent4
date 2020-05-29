1d669a665a037dc9683313d45542c8036464f7b9
Soro Cat <sorousha326@gmail.com>
2020-05-29 11:47:08 -0700

    back to normal

--- src/age.js
+++ src/age.js
@@ -1,24 +1,6 @@
 export function userAge(age) {
-  constructor (age) {
-    this.age = age;
+  this.mercuryYears = age;
+  this.venusYears = age;
+  this.marsYears = age;
+  this.jupiterYears = age;
 }
-mercuryYears(){
-  this.age = .24
-  const userAge = Math.round(this.age);
-  return userAge;
-  }
-venusYears(){
-  this.age = .62
-  const userAge = Math.round(this.age);
-  return userAge;
-  }
-marsYears(){
-  this.age = 1.88
-  const userAge = Math.round(this.age);
-  return userAge;
-  }
-jupiterYears(){
-  this.age = 11.86
-  const userAge = Math.round(this.age);
-  return userAge;
-  }
