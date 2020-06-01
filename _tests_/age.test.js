f604bce8a1c68199100d351e0fdb1faa82272fd3
Soro Cat <sorousha326@gmail.com>
2020-06-01 09:48:00 -0700

    add changes to test

--- _tests_/age.test.js
+++ _tests_/age.test.js
@@ -3,4 +3,5 @@ import { userAge } from './../src/age.js';
 describe('userAge', () => {
 
 test ('The number you put in is multiplied by the number of the fuction'() => {
-var userAge = 
+var userAge = new userAge(99);
+console.log(age);
