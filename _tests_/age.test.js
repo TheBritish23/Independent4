01231d1a7f865b4c36abffdb3ce67e5da472c7b5
Soro Cat <sorousha326@gmail.com>
2020-06-01 11:28:12 -0700

    add more change

--- _tests_/age.test.js
+++ _tests_/age.test.js
@@ -3,5 +3,7 @@ import { userAge } from './../src/age.js';
 describe('userAge', () => {
 
 test ('The number you put in is multiplied by the number of the fuction'() => {
-var userAge = new userAge(99);
-console.log(age);
+  var userAge = new userAge(99);
+  console.log(age);
+  expect(age.mercuryYears()).toEqual(412);
+ });
