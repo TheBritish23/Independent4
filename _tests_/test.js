8d2e504937cbd94990392f159e1cff315c7e2d68
Soro Cat <sorousha326@gmail.com>
2020-06-05 11:49:43 -0700

    fix test.js

--- _tests_/test.js
+++ _tests_/test.js
@@ -12,15 +12,18 @@ test ('The number you put in is multiplied by the number of the fuction'() => {,
   var userAge = new userAge(99);
   console.log(age);
   expect(age.venusYears()).toEqual(161);
+  return "161";
 });
 test ('The number you put in is multiplied by the number of the fuction'() => {,
   var userAge = new userAge(99);
   console.log(age);
   expect(age.marsYears()).toEqual(53);
+  return "53";
 });
 test ('The number you put in is multiplied by the number of the fuction'() => {
   var userAge = new userAge(99);
   console.log(age);
   expect(age.jupiterYears()).toEqual(9);
+  return "9";
   });
 });
