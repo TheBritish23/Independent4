92128b87f79389d43b6399fd84a4031ded45c1d8
Soro Cat <sorousha326@gmail.com>
2020-06-08 12:07:09 -0700

    fix puncuation

--- _tests_/test.js
+++ _tests_/test.js
@@ -2,25 +2,25 @@ import { userAge } from './../src/age.js';
 
 describe('userAge', () => {
 
-test ('The number you put in is multiplied by the number of the fuction'(), => {
+test ('The number you put in is multiplied by the number of the fuction', () => {
   var userAge = new userAge(99);
   console.log(age);
   expect(age.mercuryYears()).toEqual(412);
   return "412";
  });
-test ('The number you put in is multiplied by the number of the fuction'(), => {
+test ('The number you put in is multiplied by the number of the fuction', () => {
   var userAge = new userAge(99);
   console.log(age);
   expect(age.venusYears()).toEqual(161);
   return "161";
 });
-test ('The number you put in is multiplied by the number of the fuction'(), => {
+test ('The number you put in is multiplied by the number of the fuction', () => {
   var userAge = new userAge(99);
   console.log(age);
   expect(age.marsYears()).toEqual(53);
   return "53";
 });
-test ('The number you put in is multiplied by the number of the fuction'(), => {
+test ('The number you put in is multiplied by the number of the fuction', () => {
   var userAge = new userAge(99);
   console.log(age);
   expect(age.jupiterYears()).toEqual(9);
