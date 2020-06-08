28f25090d054b6c018ccd76b6f7810b916b2cf0b
Soro Cat <sorousha326@gmail.com>
2020-06-08 12:17:29 -0700

    add changes

--- _tests_/test.js
+++ _tests_/test.js
@@ -3,25 +3,25 @@ import { userAge } from './../src/age.js';
 describe('userAge', () => {
 
 test ('The number you put in is multiplied by the number of the fuction', () => {
-  var userAge = new userAge(99);
+  var calculate = new userAge(99);
   console.log(age);
   expect(age.mercuryYears()).toEqual(412);
   return "412";
  });
 test ('The number you put in is multiplied by the number of the fuction', () => {
-  var userAge = new userAge(99);
+  var calculate = new userAge(99);
   console.log(age);
   expect(age.venusYears()).toEqual(161);
   return "161";
 });
 test ('The number you put in is multiplied by the number of the fuction', () => {
-  var userAge = new userAge(99);
+  var calculate = new userAge(99);
   console.log(age);
   expect(age.marsYears()).toEqual(53);
   return "53";
 });
 test ('The number you put in is multiplied by the number of the fuction', () => {
-  var userAge = new userAge(99);
+  var calculate = new userAge(99);
   console.log(age);
   expect(age.jupiterYears()).toEqual(9);
   return "9";
