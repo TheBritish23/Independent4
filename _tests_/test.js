12dbc218e1dca856e38af9f37000eb83a4f6dc8f
Soro Cat <sorousha326@gmail.com>
2020-06-09 11:31:09 -0700

    add changes

--- _tests_/test.js
+++ _tests_/test.js
@@ -4,25 +4,25 @@ describe('userAge', () => {
 
 test ('The number you put in is multiplied by the number of the fuction', () => {
   var calculate = new userAge(99);
-  console.log(age);
+  console.log(mercuryYears);
   expect(age.mercuryYears()).toEqual(412);
   return "412";
  });
 test ('The number you put in is multiplied by the number of the fuction', () => {
   var calculate = new userAge(99);
-  console.log(age);
+  console.log(venusYears);
   expect(age.venusYears()).toEqual(161);
   return "161";
 });
 test ('The number you put in is multiplied by the number of the fuction', () => {
   var calculate = new userAge(99);
-  console.log(age);
+  console.log(marsYears);
   expect(age.marsYears()).toEqual(53);
   return "53";
 });
 test ('The number you put in is multiplied by the number of the fuction', () => {
   var calculate = new userAge(99);
-  console.log(age);
+  console.log(juoiterYears);
   expect(age.jupiterYears()).toEqual(9);
   return "9";
   });
