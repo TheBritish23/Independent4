56dc1a802f54cf33526e13a09edc9e5e1932e092
Soro Cat <sorousha326@gmail.com>
2020-06-16 10:17:24 -0700

    add new prototype

--- _tests_/test.js
+++ _tests_/test.js
@@ -24,16 +24,21 @@ test ('The number you put in is multiplied by the number of the fuction', () => 
 describe('HowLongUGotLeftToLive', () => {
   test('should return number of Mercury years past expectancy if Earth user enters age that is higher than life expectancy', () => {
       var calculator = new userAge(100, 99);
-      console.log(calculator);
+      console.log(calculate);
       expect(calculator.lifeExpectancyInMercury()).toEqual(618);
     });
   test('should return number of Venus years past expectancy if Earth user enters age that is higher than life expectancy', () => {
       var calculator = new ExpectancyCalculator(100, 99);
-      console.log(calculator);
+      console.log(calculate);
       expect(calculator.lifeExpectancyInVenus()).toEqual(149);
     });
   test('should return number of Mars years past expectancy if Earth user enters age that is higher than life expectancy', () => {
       var calculator = new ExpectancyCalculator(100, 99);
-      console.log(calculator);
+      console.log(calculate);
       expect(calculator.lifeExpectancyInMars()).toEqual(53);
     });
+  test('should return number of Jupiter years past expectancy if Earth user enters age that is higher than life expectancy', () => {
+    var calculator = new ExpectancyCalculator(100, 99);
+    console.log(calculate);
+    expect(calculator.lifeExpectancyInJupiter()).toEqual(9);
+  });
