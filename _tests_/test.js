15d6a96d1227bfa57a4e8bcb9bb3d4e58f4f970b
Soro Cat <sorousha326@gmail.com>
2020-06-17 11:02:41 -0700

    change variables

--- _tests_/test.js
+++ _tests_/test.js
@@ -32,17 +32,17 @@ describe('HowLongUGotLeftToLive', () => {
       expect(calculator.lifeExpectancyInMercury()).toEqual(618);
     });
   test('should return number of Venus years past expectancy if Earth user enters age that is higher than life expectancy', () => {
-      var calculate = new ExpectancyCalculator(100, 99);
+      var calculate = new userAge(100, 99);
       console.log(calculate);
       expect(calculate.lifeExpectancyInVenus()).toEqual(149);
     });
   test('should return number of Mars years past expectancy if Earth user enters age that is higher than life expectancy', () => {
-      var calculate = new ExpectancyCalculator(100, 99);
+      var calculate = new userAge(100, 99);
       console.log(calculate);
       expect(calculate.lifeExpectancyInMars()).toEqual(53);
     });
   test('should return number of Jupiter years past expectancy if Earth user enters age that is higher than life expectancy', () => {
-    var calculate = new ExpectancyCalculator(100, 99);
+    var calculate = new userAge(100, 99);
     console.log(calculate);
     expect(calculate.lifeExpectancyInJupiter()).toEqual(9);
   });
