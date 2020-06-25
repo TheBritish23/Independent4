66a797c235d0a84cb059ad5fb1abada758c33892
Soro Cat <sorousha326@gmail.com>
2020-06-25 12:01:13 -0700

    add changes

--- src/age.js
+++ src/age.js
@@ -22,23 +22,23 @@ userAge.prototype.jupiterYears = function(){
   return jupiterYears;
 };
 
-// export function ExpectancyCalculator(averageLifeExpectancy, userAge) {
-//   this.averageLifeExpectancy = averageLifeExpectancy;
-//   this.userAge = userAge;
-// }
-// ExpectancyCalculator.prototype.lifeExpectancyInMercury = function() {
-//   let mercuryYears = Math.floor(this.averageLifeExpectancy / mercuryYears - this.userAge / mercuryYears);
-//   return mercuryYears;
-// };
-// ExpectancyCalculator.prototype.lifeExpectancyInVenus = function() {
-//   let venusYears = Math.floor(this.averageLifeExpectancy / venusYears - this.userAge / venusYears);
-//   return venusYears;
-// };
-// ExpectancyCalculator.prototype.lifeExpectancyInMars = function() {
-//   let marsYears = Math.floor(this.averageLifeExpectancy / marsYears - this.userAge / marsYears);
-//   return marsYears;
-// };
-// ExpectancyCalculator.prototype.lifeExpectancyInJupiter = function() {
-//   let jupiterYears = Math.floor(this.averageLifeExpectancy / jupiterYears - this.userAge / jupiterYears);
-//   return jupiterYears;
-// };
+export function ExpectancyCalculator(averageLifeExpectancy, userAge) {
+  this.averageLifeExpectancy = averageLifeExpectancy;
+  this.userAge = userAge;
+}
+ExpectancyCalculator.prototype.lifeExpectancyInMercury = function() {
+  let mercuryYears = Math.floor(this.averageLifeExpectancy / mercuryYears - this.userAge / mercuryYears);
+  return mercuryYears;
+};
+ExpectancyCalculator.prototype.lifeExpectancyInVenus = function() {
+  let venusYears = Math.floor(this.averageLifeExpectancy / venusYears - this.userAge / venusYears);
+  return venusYears;
+};
+ExpectancyCalculator.prototype.lifeExpectancyInMars = function() {
+  let marsYears = Math.floor(this.averageLifeExpectancy / marsYears - this.userAge / marsYears);
+  return marsYears;
+};
+ExpectancyCalculator.prototype.lifeExpectancyInJupiter = function() {
+  let jupiterYears = Math.floor(this.averageLifeExpectancy / jupiterYears - this.userAge / jupiterYears);
+  return jupiterYears;
+};
