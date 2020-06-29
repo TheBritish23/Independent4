c3ce06f2c4e51cad177c98af46313b0a588f5b4e
Soro Cat <sorousha326@gmail.com>
2020-06-29 10:39:43 -0700

    add new chages

--- src/age.js
+++ src/age.js
@@ -26,19 +26,19 @@ export function ExpectancyCalculator(averageLifeExpectancy, userAge) {
   this.averageLifeExpectancy = averageLifeExpectancy;
   this.userAge = userAge;
 }
-ExpectancyCalculator.prototype.lifeExpectancyInMercury = function() {
-  let mercuryYears = Math.floor(this.averageLifeExpectancy / mercuryYears - this.userAge / mercuryYears);
+ExpectancyCalculator.prototype.averagelifeExpectancy = function() {
+  let mercuryYears = Math.floor(this.lifeExpectancyInMercury / mercuryYears - this.userAge / mercuryYears);
   return mercuryYears;
 };
-ExpectancyCalculator.prototype.lifeExpectancyInVenus = function() {
-  let venusYears = Math.floor(this.averageLifeExpectancy / venusYears - this.userAge / venusYears);
+ExpectancyCalculator.prototype.averagelifeExpectancy = function() {
+  let venusYears = Math.floor(this.lifeExpectancyInVenus / venusYears - this.userAge / venusYears);
   return venusYears;
 };
-ExpectancyCalculator.prototype.lifeExpectancyInMars = function() {
-  let marsYears = Math.floor(this.averageLifeExpectancy / marsYears - this.userAge / marsYears);
+ExpectancyCalculator.prototype.averagelifeExpectancy = function() {
+  let marsYears = Math.floor(this.lifeExpectancyInMars / marsYears - this.userAge / marsYears);
   return marsYears;
 };
-ExpectancyCalculator.prototype.lifeExpectancyInJupiter = function() {
-  let jupiterYears = Math.floor(this.averageLifeExpectancy / jupiterYears - this.userAge / jupiterYears);
+ExpectancyCalculator.prototype.averagelifeExpectancy = function() {
+  let jupiterYears = Math.floor(this.lifeExpectancyInJupiter / jupiterYears - this.userAge / jupiterYears);
   return jupiterYears;
 };
