9e7dae57a0cb1639bb5144a693c93ea614d120a0
Soro Cat <sorousha326@gmail.com>
2020-07-06 09:59:45 -0700

    add fixes

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import {userAge} from "./age.js";
 
 $(document).ready(function(){
   $("form#Age1").submit(function(event) {
     event.preventDefault();
     const userAge = parseInt($("#userAge").val());
     if (Math.ceil(userAge)-userAge !==0 || userAge<0){
-      // alert("Invalid Input, *****!");
     } else {
       const age = new userAge(age);
       $("#mercuryYears").text(age.userAge());
